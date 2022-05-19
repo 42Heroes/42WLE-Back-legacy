a377959a45096285dd8bd3da96c4373be1e946cf
SeoJunhwan <seojunhwan@kakao.com>
2022-05-19 15:31:06 +0900

    WE-30 몽고 DB 스키마 생성

---
+++ schemas/comment/comment.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { User } from './user.schema';
+
+export type CommentDocument = Comment & Document;
+
+@Schema()
+export class Comment {
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  author: User;
+
+  @Prop({ required: true })
+  createdAt: Date;
+
+  @Prop({ required: true })
+  updatedAt: Date;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  })
+  likes: User[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  })
+  comments: Comment[];
+
+  @Prop({ required: true, default: false })
+  isDeleted: boolean;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
