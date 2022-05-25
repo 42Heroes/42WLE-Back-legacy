f933c5b153c9c7fb2322a740d21ea77ccac852b8
SeoJunhwan <seojunhwan@kakao.com>
2022-05-25 19:46:36 +0900

    WE-65 user controller, service 수정
    getUser 검색 방식 intra_id => documentId 변경
    updateUser dto 에 맞게끔 수정
    --------------
    createUser 컨트롤러 항목 제거 (oAuth 로그인 시 회원가입하기에 불필요)
    updateUser 컨트롤러 arg 추가 및 적용
    getMyInfo 컨트롤러 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { User, UserDocument } from '../schemas/user/user.schema';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { GetUser } from 'src/auth/get-user.decorator';
@@ -20,13 +9,6 @@ import { GetUser } from 'src/auth/get-user.decorator';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async createUser(
-    @Body() createUserDto: CreateUserDto,
-  ): Promise<UserDocument> {
-    return this.userService.createUser(createUserDto);
-  }
-
   @Get()
   async getAllUser(): Promise<User[]> {
     return this.userService.getAllUser();
@@ -34,44 +16,21 @@ export class UserController {
 
   @Get('/me')
   @UseGuards(JwtAuthGuard)
-  async getMyInfo(@GetUser() user) {
+  async getMyInfo(@GetUser() user: UserDocument) {
     return this.userService.getOneUser(user.intra_id);
+  }
+
+  @Patch('/me')
+  @UseGuards(JwtAuthGuard)
+  async updateUser(
+    @Body() updateUserDto: UpdateUserDto,
+    @GetUser() user: UserDocument,
+  ) {
+    return this.userService.updateUser(updateUserDto, user.id);
   }
 
   @Get('/:id')
   async getOneUser(@Param('id') userId: string) {
     return this.userService.getOneUser(userId);
   }
-
-  @Patch('/me')
-  async updateUser(@Body() updateUserDto: UpdateUserDto) {
-    return this.userService.updateUser(updateUserDto);
-  }
-
-  // @Delete('/:id')
-  // remove(@Param('id') userId: number) {
-  //   return this.userService.deleteOne(userId);
-  // }
-
-  // @Get()
-  // findAll(): User[] {
-  //   return this.userService.findAll();
-  // }
-
-  // @Get('/:id')
-  // getOne(@Param('id') userId: number): User {
-  //   return this.userService.getOne(userId);
-  // }
-
-  // @Post()
-  // create(@Body() userData: any) {
-  //   // any 부분은 dto를 정의하면서 채웁니다
-  //   return this.userService.create(userData);
-  // }
-
-  // @Patch('/:id')
-  // patch(@Param('id') userId: number, @Body() updateData: any) {
-  //   // any 부분은 dto를 정의하면서 채웁니다
-  //   return this.userService.update(userId, updateData);
-  // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,16 +14,28 @@ export class UserService {
   async createUser(createUserDto: CreateUserDto): Promise<UserDocument> {
     const newUser = new this.userModel(createUserDto);
     const result = await newUser.save();
-    // console.log(result);
+
     return result as UserDocument;
   }
 
-  async getOneUser(intra_id: string): Promise<UserDocument> {
-    const user = await this.userModel.findOne({ intra_id });
+  async getOneUser(userId: string): Promise<UserDocument> {
+    const user = await this.userModel.findById(userId);
+
     if (!user) {
-      throw new NotFoundException(`Can't find user ${intra_id}`);
+      throw new NotFoundException(`Can't find user ${userId}`);
     }
+
     return user;
+  }
+
+  async getAllUser(): Promise<User[]> {
+    const allUser = await this.userModel.find().exec();
+
+    if (!allUser) {
+      throw new NotFoundException(`Can't find`);
+    }
+
+    return allUser;
   }
 
   async isUser(intra_id: string): Promise<UserDocument> {
@@ -34,22 +46,16 @@ export class UserService {
     return user;
   }
 
-  async getAllUser(): Promise<User[]> {
-    const allUser = await this.userModel.find().exec();
-    if (!allUser) {
-      throw new NotFoundException(`Can't find`);
-    }
-    return allUser;
-  }
+  async updateUser(
+    updateUserDto: UpdateUserDto,
+    userId: string,
+  ): Promise<UserDocument> {
+    const updatedUser = await this.userModel.findOneAndUpdate(
+      { _id: userId },
+      { ...updateUserDto, isRegisterDone: true },
+      { new: true },
+    );
 
-  async updateUser(updateUserDto: UpdateUserDto): Promise<User> {
-    //TODO: Auth JWT 로 변경 예정
-    const user = await this.getOneUser(updateUserDto.id);
-
-    const { id, ...rest } = updateUserDto;
-    // console.log(rest);
-    await user.updateOne(rest);
-
-    return user;
+    return updatedUser;
   }
 }
