684532b3642677486caab9f82419be657aa95adf
SeoJunhwan <seojunhwan@kakao.com>
2022-05-25 22:11:13 +0900

    WE-65 cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+      credentials: true,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
