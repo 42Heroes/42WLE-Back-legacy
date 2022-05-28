dbae3d36c0955435c63170e04a97e0fbe68784e6
SeoJunhwan <seojunhwan@kakao.com>
2022-05-29 00:35:04 +0900

    WE-60 chatRoom schema 수정
    조금 이상하게 되어있길래 수정합니다.

--- src/schemas/chatRoom/chatRoom.schema.ts
+++ src/schemas/chatRoom/chatRoom.schema.ts
@@ -6,33 +6,17 @@ export type ChatRoomDocument = ChatRoom & Document;
 
 @Schema()
 export class ChatRoom {
-  @Prop({
-    required: true,
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  })
-  author: User;
-
-  @Prop({ required: true })
+  @Prop({ required: true, default: new Date() })
   createdAt: Date;
 
-  @Prop({ required: true })
+  @Prop({ required: true, default: new Date() })
   updatedAt: Date;
-
-  @Prop({ required: true })
-  content: string;
 
   @Prop({
     required: true,
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   })
-  likes: User[];
-
-  @Prop({ required: true, default: false })
-  isDeleted: boolean;
-
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] })
-  comments: Comment[];
+  users: User[];
 }
 
 export const ChatRoomSchema = SchemaFactory.createForClass(ChatRoom);
